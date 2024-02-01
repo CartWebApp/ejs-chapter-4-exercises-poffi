f069e34aed3ce80164a4a141286d2d40d33795a6
poffi <143832574+poffi@users.noreply.github.com>
2024-02-01 10:42:55 -0800

    saved

--- reverse-arrays.js
+++ reverse-arrays.js
@@ -1,20 +1,31 @@
-function reverseArray(arr) {
- '1','2','3','4'
+
+function reverseArray(inputArray) {
+    let outputArray = []
+    for(let i = inputArray.length - 1; i >= 0 ; i--){
+        outputArray.push(inputArray[i]);
+    }
+    return outputArray;
+}
+console.log(reverseArray(['A','B','C']));
+
+function reverseArrayInPlace(inputArray) {
+    let outputArray = []
+    for(let i = inputArray.length - 1; i >= 0 ; i--){
+        outputArray.push(inputArray[i]);
+    }
+    return outputArray;
+
 }
 
-function reverseArrayInPlace(arr) {
- '1','2','3','4'
-}
 
-
-
-
-
-
-// tests 
-console.log(reverseArray(["A", "B", "C"]));
-// → ["C", "B", "A"];
-let arrayValue = [1, 2, 3, 4, 5];
+let arrayValue = [];
 reverseArrayInPlace(arrayValue);
-console.log(arrayValue);
+console.log(arrayValue([1, 2, 3, 4, 5,]));
+
+// → ["C", "B", "A"];
+
+
+
 // → [5, 4, 3, 2, 1]
+
+
