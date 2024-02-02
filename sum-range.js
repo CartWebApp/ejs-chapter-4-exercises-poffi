3d68bc0906ae6dac0e556cdbd59a39a9ff29e9f1
poffi <143832574+poffi@users.noreply.github.com>
2024-02-02 10:41:21 -0800

    saved

--- sum-range.js
+++ sum-range.js
@@ -1,24 +1,37 @@
 function range(start, end, step = 1) {
-  let range=[1]
-  for(let i = range.length = 1; i <= 10 ; i+= range.length){
-    range.push(range);
+  let range= [];
+  if(step < 0) {
+
+  } else
+  if (step > 0) {
+    for( let i = start; i>= end; i -= step);
+    range.push
+
+  for(let i = start; i <= end; i += step){
+    range.push(i);
 }
-return range=[]
+return range
+}
 }
 
 function sum(numbers) {
-  // Add code.
+  let sum= 0, range = 1;
+while (range <= 10) {
+  sum += range;
+  range += 1;
 }
+return range=55
+}
+console.log(sum(range(1, 10)));
 
 
 
 
 
 
-// tests
 console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
-console.log(sum(range(1, 10)));
-// → 55
+
+
